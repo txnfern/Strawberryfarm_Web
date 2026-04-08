1bb4c445209c9738c1403ccbf7a8b5dfadfc8d9f
txnfern <chonthichaphumphung@gmail.com>
2026-04-08 22:34:28 +0700

    update login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
       return;
     }
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("https://strawberryfarm-web.vercel.app/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
