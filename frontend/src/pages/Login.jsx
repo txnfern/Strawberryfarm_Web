c81d331358893ee11731786b4821d520804f8223
txnfern <chonthichaphumphung@gmail.com>
2026-04-09 15:53:48 +0700

    edit runder

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://strawberryfarm-web.vercel.app/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
       return;
     }
     try {
-      const response = await fetch("https://strawberryfarm-web.vercel.app/register", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
