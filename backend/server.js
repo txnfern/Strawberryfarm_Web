b0e89974713899fa89db8aa3561cbd89760cee42
txnfern <chonthichaphumphung@gmail.com>
2026-04-09 15:21:13 +0700

    15.20 9/4/69

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 
 const app = express();
 app.use(cors({
